2a1429c2a4706cfa7a210854dec0bf169d2cbbca
Akhil Vinayak P <46107438+AkhilVinayakp@users.noreply.github.com>
2019-05-04 12:20:50 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+var express =require('express');
+var app=new express();
+var ejs=require('ejs');
+var engine=require('ejs-mate');
+app.engine('ejs',engine);
+app.set('view engine','ejs');
+app.get('/',(req,res,next)=>{
+    res.render('index');
+})
+app.listen(3000,function(){
+    console.log("this is just a test request listening ,,,,,,,,,,");
+})
